11650cf9c42bb3909097d15d72b3cd9e9297d832
pagtamajr@gmail.com <pagtamajr@gmail.com>
2020-08-18 17:45:07 -0700

    Server setup

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,1 +1,13 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express(),
+port = process.env.SERVER_PORT || 5501;
+
+app.use(cors());
+app.use(express.json());
+
+app.listen(port, () => console.log(`Connection established on port: ${port}`));
