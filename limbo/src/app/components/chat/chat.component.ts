a4dc4277590a16bc349d3f4efe8f1f964391e81f
pagtamajr@gmail.com <pagtamajr@gmail.com>
2020-08-22 21:48:32 -0700

    Chat layout update

--- limbo/src/app/components/chat/chat.component.ts
+++ limbo/src/app/components/chat/chat.component.ts
@@ -6,9 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./chat.component.scss']
 })
 export class ChatComponent implements OnInit {
-
+  servers: string[] = ['💩💩💩💩💩'];
   constructor() { }
-
+  
   ngOnInit(): void {
   }
 
