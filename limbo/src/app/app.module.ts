1f384aecdf0eecfc6b414df73ecac43db7a6b254
pagtamajr@gmail.com <pagtamajr@gmail.com>
2020-08-25 01:26:04 -0700

    Message box update

--- limbo/src/app/app.module.ts
+++ limbo/src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ChatComponent } from './components/chat/chat.component';
 import { SigninComponent } from './components/signin/signin.component';
+import { SearchComponent } from './components/shared/search/search.component';
+import { MessageComponent } from './components/shared/message/message.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ChatComponent,
-    SigninComponent
+    SigninComponent,
+    SearchComponent,
+    MessageComponent
   ],
   imports: [
     BrowserModule,
