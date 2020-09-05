0d0aa7443257c1a01193efa277c187566e51f4fd
pagtamajr@gmail.com <pagtamajr@gmail.com>
2020-09-04 23:48:12 -0700

    Created main header and footer components

--- limbo/src/app/app.module.ts
+++ limbo/src/app/app.module.ts
@@ -8,6 +8,8 @@ import { SigninComponent } from './components/signin/signin.component';
 import { SearchComponent } from './components/shared/search/search.component';
 import { MessageComponent } from './components/shared/message/message.component';
 import { HomeComponent } from './components/home/home.component';
+import { MainHeaderComponent } from './components/shared/main-header/main-header.component';
+import { MainFooterComponent } from './components/shared/main-footer/main-footer.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +18,9 @@ import { HomeComponent } from './components/home/home.component';
     SigninComponent,
     SearchComponent,
     MessageComponent,
-    HomeComponent
+    HomeComponent,
+    MainHeaderComponent,
+    MainFooterComponent
   ],
   imports: [
     BrowserModule,
