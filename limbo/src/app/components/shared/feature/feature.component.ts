dadf8797fbed97b6b6f21278ff072b7118ba2d87
pagtamajr@gmail.com <pagtamajr@gmail.com>
2020-09-29 23:38:10 -0700

    Updated feature component

--- limbo/src/app/components/shared/feature/feature.component.ts
+++ limbo/src/app/components/shared/feature/feature.component.ts
@@ -7,7 +7,11 @@ import { Feature } from '../../../interfaces/feature';
   styleUrls: ['./feature.component.scss']
 })
 export class FeatureComponent implements OnInit {
-  @Input() features: Feature[];
+  featureData: Feature[] = [
+    { className: 'layout-home__feature-1', heading: 'Join a community or start your own', feature1: 'Manage members and grant special privileges.', feature2: 'Create private channels and many more.' },
+    { className: 'layout-home__feature-2', heading: 'Easily keep in touch', feature1: 'Chat with old friends or make new ones.', feature2: 'Friends are notified when you are connected.' },
+    { className: 'layout-home__feature-3', heading: 'A place to meet like minded individuals', feature1: 'Discuss your interests', feature2: 'Share ideas' },
+  ];
 
   // classes: {}[] = [
   //   { className: 'layout-home__feature-1' },
