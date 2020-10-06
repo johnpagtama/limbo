acd5bade78e80459b35d174bcbb96c83b8ff936c
pagtamajr@gmail.com <pagtamajr@gmail.com>
2020-10-05 20:36:47 -0700

    Refactored jump nav component

--- limbo/src/app/components/shared/jump-nav/jump-nav.component.ts
+++ limbo/src/app/components/shared/jump-nav/jump-nav.component.ts
@@ -6,7 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./jump-nav.component.scss']
 })
 export class JumpNavComponent implements OnInit {
-
+  jumpNavData: any[] = [
+    { gridArea: 'jump-nav__anchor1', classes: 'jump-nav__txt-dec-none', link: '#feature1', text: 'Section 1' },
+    { gridArea: 'jump-nav__anchor2', classes: 'jump-nav__txt-dec-none', link: '#feature2', text: 'Section 2' },
+    { gridArea: 'jump-nav__anchor3', classes: 'jump=nav__txt-dec-none', link: '#feature3', text: 'Section 3' }
+  ];
   constructor() { }
 
   ngOnInit(): void {
